6e2bdddc295257738092bc0f189dc3eedbd7431b
José Roberto Ferreira <joseroberto.ferreira@gmail.com>
2024-11-04 15:40:28 +0000

    Review

--- api_orders/messages/receive.py
+++ api_orders/messages/receive.py
@@ -6,18 +6,16 @@ import requests
 class ListQueuedMessages:
     def get_messages(base_url, receiver, headers):
         # Implementa a lógica para recuperar as mensagens
-        service_url = (
-            f'https://{base_url}/ListQueuedMessageIds?Receiver={receiver}'
-        )
+        service_url = f'https://{base_url}/ListQueuedMessageIds?Receiver={receiver}'
 
         response = requests.get(service_url, headers=headers)
 
         json_response = json.loads(response.text)
 
-        # Reduz o número de IDs de mensagem para os primeiros 2
-        json_response['ResultData']['MessageIds'] = json_response[
-            'ResultData'
-        ]['MessageIds'][:2]
+        # Reduz o número de IDs de mensagens
+        # json_response['ResultData']['MessageIds'] = json_response[
+        #     'ResultData'
+        # ]['MessageIds'][:2]
 
         return {
             'CorrelationId': json_response['CorrelationId'],
